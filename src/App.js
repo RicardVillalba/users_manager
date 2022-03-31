bb5d15e0859dc3b0cdb1afd19a68c579c9a43379
RicardVillalba <ricardsax@gmail.com>
2022-03-31 12:27:28 +0200

    card component and container component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import useForm from "./hooks/useForm";
 import Input from "./components/Input";
+import Card from "./components/Card";
+import Container from "./components/Container";
 
 function App() {
   const [form, handleChange] = useForm({ name: "", lastName: "" });
 
   console.log(form);
   return (
-    <form>
-      <Input
-        label="name"
-        name="name"
-        value={form.name}
-        onChange={handleChange}
-      />
-      <Input
-        label="Last name"
-        name="lastName"
-        value={form.lastName}
-        onChange={handleChange}
-      />
-    </form>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="name"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Last name"
+              name="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
