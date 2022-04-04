075adcfce6960b83586d577c77fce9291a5dcf3f
RicardVillalba <ricardsax@gmail.com>
2022-04-04 17:07:26 +0200

    using usestate to save users on submit function greated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useForm from "./hooks/useForm";
 import Input from "./components/Input";
 import Card from "./components/Card";
@@ -5,14 +6,20 @@ import Container from "./components/Container";
 import Button from "./components/Button";
 
 function App() {
+  const [users, setUsers] = useState([]);
   const [form, handleChange] = useForm({ name: "", lastName: "", email: "" });
 
-  console.log(form);
+  const submit = (e) => {
+    e.preventDefault();
+    setUsers([...users, form]);
+  };
+
+  console.log(form, users);
   return (
     <Container>
       <Card>
         <div style={{ padding: 20 }}>
-          <form>
+          <form onSubmit={submit}>
             <Input
               label="name"
               name="name"
@@ -31,8 +38,8 @@ function App() {
               value={form.email}
               onChange={handleChange}
             />
+            <Button>send</Button>
           </form>
-          <Button>send</Button>
         </div>
       </Card>
     </Container>
